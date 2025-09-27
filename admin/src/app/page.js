a226a5748821a29c672401f2c0d33b9efd67d5a9
vrushabh <rushab361@gmail.com>
2025-09-27 18:09:01 +0530

    intial commit

--- admin/src/app/page.js
+++
@@ -1,9 +0,0 @@
-import OverviewPage from "./overview/page";
-
-export default function Home() {
-  return (
-    <div>
-      <OverviewPage />
-    </div>
-  );
-}
--- admin/src/app/static/staticPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { db } from "../../../config/db";
-
-export const StaticPage = async () => {
-  const data = await db.execute("select * from user");
-  console.log(data);
-  return <div>staticPage</div>;
-};
