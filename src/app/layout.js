872ec25a6d9f0b82baa03626c973a6316d1c2366
Guruprasad <guruprasad1736@gmail.com>
2025-11-14 19:47:58 +0530

    Refactor code structure for improved readability and maintainability

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,6 +23,10 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+        <title>RevUp Admin</title>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
